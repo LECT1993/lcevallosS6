98e3221ea43dd799f866e7e39c13c228b9a70d6c
LECT <LECT@DESKTOP-8TV9SIH>
2023-07-03 21:53:17 -0500

    Agregar archivos de proyecto.

---
+++ lcevallosS5a/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lcevallosS5a
+{
+    public class Estudiante
+    {
+        public int codigo { set; get; } 
+        public string nombre { set; get; }
+        public string apellido { set; get; }
+        public string edad { set; get; }
+    }
+}
